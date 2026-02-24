a80a60de188d86a154d618f1627f202eec058e13
Vinicius Rodrigues <viniciusysrodrigues@gmail.com>
2026-02-25 05:57:17 +0800

    Google Analytics

--- src/app/GoogleAnalytics.tsx
+++ src/app/GoogleAnalytics.tsx
@@ -1,18 +1,26 @@
 import Script from "next/script";
 
 export default function GoogleAnalytics() {
+
+    const measurementId = process.env.NEXT_PUBLIC_MEASUREMENT_ID;
+
+    if (!measurementId) {
+        console.warn("Google Analytics Measurement ID is not set.");
+        return null;
+    }
+    
   return (
     <>
       <Script
         strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}
       />
       <Script id="google-analytics" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}');
+          gtag('config', '${measurementId}');
         `}
       </Script>
     </>
